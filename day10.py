8fbfb02afeb9f366fa2d5763d7dc0117471fb92e
Rasmus Öhman Bruno <rasmus@ohmanbruno.se>
2021-12-10 06:37:40 +0100

    day10 fix

--- day10.py
+++ day10.py
@@ -1,9 +1,11 @@
 lines = open("data/day10.txt").read().splitlines()
 
+
 matching = {"(": ")", "[": "]", "{": "}", "<": ">"}
+completion_points = {")": 1, "]": 2, "}": 3, ">": 4}
 
 
-def points_line(line):
+def corrupted_score(line):
     stack = [line[0:1]]
     for char in line[1:]:
         if char in ["(", "[", "{", "<"]:
@@ -23,7 +25,7 @@ def points_line(line):
     return 0
 
 
-def completion(line):
+def completion_score(line):
     stack = [line[0:1]]
     for char in line[1:]:
         if char in ["(", "[", "{", "<"]:
@@ -33,24 +35,17 @@ def completion(line):
     completion = []
     while len(stack) > 0:
         completion.append(matching[stack.pop()])
-    # print(line," ", completion)
     score = 0
-    completion_points = {")": 1, "]": 2, "}": 3, ">": 4}
     for ch in completion:
         score *= 5
         score += completion_points[ch]
     return score
 
 
-points = 0
-for line in lines:
-    points += points_line(line)
-print(points)
-"part 2"
+print(sum([corrupted_score(line) for line in lines]))
 
-incomplete = [line for line in lines if points_line(line) == 0]
-points = []
-for line in incomplete:
-    points.append(completion(line))
-points.sort()
-print(points[len(points)//2])
+"part 2"
+completion_scores = [completion_score(
+    line) for line in lines if corrupted_score(line) == 0]
+completion_scores.sort()
+print(completion_scores[len(completion_scores)//2])
