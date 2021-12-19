c5be52a1c8201d9cb974b6ca515fabd2e93d4330
Rasmus Öhman Bruno <rasmus@ohmanbruno.se>
2021-12-19 08:41:30 +0100

    day 18

--- day18.py
+++ day18.py
@@ -1,76 +1,79 @@
+import itertools
 import math
+import copy
 
 
 class Node:
-    def __init__(self, parent=None, val=None):
-        self.parent = parent
-        self.value = val
-
-    def root(self):
-        root = self
-        while root.parent != None:
-            root = root.parent
-        return root
-
-    def valuenodes(self):
-        if type(self.value) == tuple:
-            yield from self.value[0].valuenodes()
-            yield from self.value[1].valuenodes()
-        else:
-            yield self.value
+    def __init__(self, val):
+        self.val = val
 
     def __str__(self):
-        if type(self.value) == tuple:
-            return "({0},{0})".format(str(self.value[0]), str(self.value[1]))
+        return self.to_string()
+
+    def to_string(self):
+        if type(self.val) == tuple:
+            return "({},{})".format(
+                self.val[0].to_string(), self.val[1].to_string())
         else:
-            return str(self.value)
+            return str(self.val)
+
+    def magnitude(self):
+        if type(self.val) == tuple:
+            return 3*self.val[0].magnitude()+2*self.val[1].magnitude()
+        else:
+            return self.val
 
 
-def parse(s, parent=None):
+def valuenodes(node):
+    if type(node.val) == tuple:
+        yield from valuenodes(node.val[0])
+        yield from valuenodes(node.val[1])
+    else:
+        yield node
+
+
+def parse(s):
     if s[0] == "[":
-        node = Node(parent)
-        l, rest = parse(s[1:], node)
-        r, rest = parse(rest[1:], node)
-        node.value = (l, r)
-        return node, rest[1:]
-    return Node(parent, int(s[0])), s[1:]
+        l, rest = parse(s[1:])
+        r, rest = parse(rest[1:])
+        return Node((l, r)), rest[1:]
+    return Node(int(s[0])), s[1:]
 
 
 def split(node):
-    if type(node.value) == tuple:
-        splitted = split(node.value[0])
+    if type(node.val) == tuple:
+        splitted = split(node.val[0])
         if splitted:
             return True
-        return split(node.value[1])
+        return split(node.val[1])
     else:
-        if node.value > 10:
-            node.value = (Node(node, node.value//2),
-                          Node(node, math.ceil(node.value/2)))
+        if node.val > 9:
+            node.val = (Node(node.val//2), Node(math.ceil(node.val/2)))
+            return True
+        return False
 
 
-def explode(node, level=0):
-    if type(node.value) == tuple:
+def explode(node, vnodes=None, level=0):
+    if vnodes == None:
+        vnodes = list(valuenodes(node))
+    if type(node.val) == tuple:
         if level >= 4:
-            vnodes = list(node.root().valuenodes())
-            i = vnodes.index(node.value[0])
+            i = vnodes.index(node.val[0])
             if i > 0:
-                vnodes[i-1].value += node.value[0]
-            if i < len(vnodes)-1:
-                vnodes[i+2].value += node.value[1]
-
-            if node.parent.value[0] == node:
-                node.parent.value[0] = Node(node.parent, 0)
-            else:
-                node.parent.value[1] = Node(node.parent, 0)
+                vnodes[i-1].val += node.val[0].val
+            if i+2 < len(vnodes):
+                vnodes[i+2].val += node.val[1].val
+            node.val = 0
             return True
         else:
-            if explode(node.value[0], level+1):
+            if explode(node.val[0], vnodes, level+1):
                 return True
-            return explode(node.value[1])
+            return explode(node.val[1], vnodes, level+1)
     return False
 
 
-def reduce(node):
+def add(exp1, exp2):
+    node = Node((copy.deepcopy(exp1), copy.deepcopy(exp2)))
     while True:
         exploded = explode(node)
         if exploded:
@@ -78,22 +81,15 @@ def reduce(node):
         splitted = split(node)
         if not splitted:
             break
-
-
-def add(exp1, exp2):
-    added = (exp1, exp2)
-    return reduce(added)
-
-
-def magnitude(exp):
-    pass
+    return node
 
 
 lines = open("data/day18.txt").read().splitlines()
-for line in lines:
-    p, _ = parse([c for c in line])
-    for i in p.valuenodes():
-        print(p)
-    # reduce(p)
-    # print(p)
-    print()
+nrs = [parse(line)[0] for line in lines]
+total = nrs[0]
+for nr in nrs[1:]:
+    total = add(total, nr)
+print(total.magnitude())
+print("part 2")
+print(max([add(a, b).magnitude()
+      for (a, b) in itertools.permutations(nrs, 2)]))
